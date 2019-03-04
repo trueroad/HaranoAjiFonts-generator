d95fc5ec6cd4db6f4837124f179a86c3c7e44e7b
Masamichi Hosoda <trueroad@trueroad.jp>
2019-03-04 21:04:47 +0900

    Add error handling to `make_font.sh`

--- make_font.sh
+++ make_font.sh
@@ -14,54 +14,65 @@ TTXDIR=${BASEDIR}/ttx
 DOWNLOADDIR=${BASEDIR}/download
 
 echo deleting old files...
-rm *.ttx *.log
+rm -f *.ttx *.log \
+   || { echo error; exit 1; }
 
 echo making conversion table...
 ${BINDIR}/make_conv_table \
     ${TTXDIR}/${SRC_FONTBASE}._c_m_a_p.ttx ${DOWNLOADDIR}/${CMAP} \
-    > table.tbl 2> table.log
+    > table.tbl 2> table.log \
+   || { echo error; exit 1; }
 
 echo making glyphorder...
 ${BINDIR}/make_glyphorder \
     table.tbl \
-    > glyphorder.ttx 2> glyphorder.log
+    > glyphorder.ttx 2> glyphorder.log \
+   || { echo error; exit 1; }
 
 echo converting name table...
 ${BINDIR}/conv_name \
     ${SRC_FONTBASE} "${TOOLVER}" ${TTXDIR}/${SRC_FONTBASE}._n_a_m_e.ttx \
-    2> name.log | sed -f ${BASEDIR}/font_name.sed > name.ttx
+    2> name.log | sed -f ${BASEDIR}/font_name.sed > name.ttx \
+   || { echo error; exit 1; }
 echo converting cmap table...
 ${BINDIR}/conv_cmap \
     table.tbl ${TTXDIR}/${SRC_FONTBASE}._c_m_a_p.ttx \
-    > cmap.ttx 2> cmap.log
+    > cmap.ttx 2> cmap.log \
+   || { echo error; exit 1; }
 echo converting CFF table...
 ${BINDIR}/conv_CFF \
     table.tbl ${TTXDIR}/${SRC_FONTBASE}.C_F_F_.ttx \
-    2> CFF.log | sed -f ${BASEDIR}/font_name.sed > CFF.ttx
+    2> CFF.log | sed -f ${BASEDIR}/font_name.sed > CFF.ttx \
+   || { echo error; exit 1; }
 echo converting VORG table...
 ${BINDIR}/conv_VORG \
     table.tbl ${TTXDIR}/${SRC_FONTBASE}.V_O_R_G_.ttx \
-    > VORG.ttx 2> VORG.log
+    > VORG.ttx 2> VORG.log \
+   || { echo error; exit 1; }
 echo converting hmtx table...
 ${BINDIR}/conv_mtx \
     table.tbl ${TTXDIR}/${SRC_FONTBASE}._h_m_t_x.ttx \
-    > hmtx.ttx 2> hmtx.log
+    > hmtx.ttx 2> hmtx.log \
+   || { echo error; exit 1; }
 echo converting vmtx table...
 ${BINDIR}/conv_mtx \
     table.tbl ${TTXDIR}/${SRC_FONTBASE}._v_m_t_x.ttx \
-    > vmtx.ttx 2> vmtx.log
+    > vmtx.ttx 2> vmtx.log \
+   || { echo error; exit 1; }
 
 echo symbolic linking other tables...
-ln -s ${TTXDIR}/${SRC_FONTBASE}._h_e_a_d.ttx
-ln -s ${TTXDIR}/${SRC_FONTBASE}._h_h_e_a.ttx
-ln -s ${TTXDIR}/${SRC_FONTBASE}._m_a_x_p.ttx
-ln -s ${TTXDIR}/${SRC_FONTBASE}.O_S_2f_2.ttx
-ln -s ${TTXDIR}/${SRC_FONTBASE}._p_o_s_t.ttx
-ln -s ${TTXDIR}/${SRC_FONTBASE}.B_A_S_E_.ttx
-ln -s ${TTXDIR}/${SRC_FONTBASE}._v_h_e_a.ttx
+ln -s ${TTXDIR}/${SRC_FONTBASE}._h_e_a_d.ttx || { echo error; exit 1; }
+ln -s ${TTXDIR}/${SRC_FONTBASE}._h_h_e_a.ttx || { echo error; exit 1; }
+ln -s ${TTXDIR}/${SRC_FONTBASE}._m_a_x_p.ttx || { echo error; exit 1; }
+ln -s ${TTXDIR}/${SRC_FONTBASE}.O_S_2f_2.ttx || { echo error; exit 1; }
+ln -s ${TTXDIR}/${SRC_FONTBASE}._p_o_s_t.ttx || { echo error; exit 1; }
+ln -s ${TTXDIR}/${SRC_FONTBASE}.B_A_S_E_.ttx || { echo error; exit 1; }
+ln -s ${TTXDIR}/${SRC_FONTBASE}._v_h_e_a.ttx || { echo error; exit 1; }
 
 echo converting root ttx file...
 sed -f ${BASEDIR}/font_tables.sed ${TTXDIR}/${SRC_FONTBASE}.ttx \
-    > output.ttx
+    > output.ttx \
+   || { echo error; exit 1; }
 
-ttx -b --recalc-timestamp output.ttx
+ttx -b --recalc-timestamp output.ttx \
+   || { echo error; exit 1; }
