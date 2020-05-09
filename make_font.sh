75a56b24466704fbc4f858e0de2c2912b817f100
Masamichi Hosoda <trueroad@trueroad.jp>
2020-05-09 23:46:34 +0900

    Fix spacing make_font.sh

--- make_font.sh
+++ make_font.sh
@@ -17,13 +17,13 @@ DOWNLOADDIR=${BASEDIR}/download
 case "${SRC_FONTBASE}" in
     SourceHanSansJP* )
         FONT_LANG=JP
-	FONT_TYPE=Sans
+        FONT_TYPE=Sans
         AI0_SOURCEHAN=${DOWNLOADDIR}/AI0-SourceHanSans
         AJ1X_KANJI=${DOWNLOADDIR}/SourceHanSans/aj16-kanji.txt
         ;;
     SourceHanSerifJP* )
         FONT_LANG=JP
-	FONT_TYPE=Serif
+        FONT_TYPE=Serif
         AI0_SOURCEHAN=${DOWNLOADDIR}/AI0-SourceHanSerif
         AJ1X_KANJI=${DOWNLOADDIR}/SourceHanSerif/aj16-kanji.txt
         ;;
@@ -36,9 +36,9 @@ case "${SRC_FONTBASE}" in
         FONT_TYPE=Serif
         ;;
     * )
-	echo invalid font name
-	exit 1
-	;;
+        echo invalid font name
+        exit 1
+        ;;
 esac
 
 case "${FONT_LANG}" in
@@ -76,7 +76,7 @@ echo "making conversion table (from cmap and CMap)..."
 ${BINDIR}/make_conv_table \
     ${TTXDIR}/${SRC_FONTBASE}._c_m_a_p.ttx ${DOWNLOADDIR}/${CMAP} \
     > table-cmap.tbl 2> table-cmap.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 
 if [ "${FONT_LANG}" = "JP" ]; then
     echo "making conversion table (from JISX0208-SourceHan-Mapping.txt)..."
@@ -110,92 +110,92 @@ ${BINDIR}/make_feature_table \
     table10.tbl fwid ${TTXDIR}/${SRC_FONTBASE}.G_S_U_B_.ttx \
     ${FEATURE_GSUB_FEA} \
     > table-fwid.tbl 2> table-fwid.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 
 echo "making conversion table (OpenType feature hwid)..."
 ${BINDIR}/make_feature_table \
     table10.tbl hwid ${TTXDIR}/${SRC_FONTBASE}.G_S_U_B_.ttx \
     ${FEATURE_GSUB_FEA} \
     > table-hwid.tbl 2> table-hwid.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 
 echo "making conversion table (OpenType feature pwid)..."
 ${BINDIR}/make_feature_table \
     table10.tbl pwid ${TTXDIR}/${SRC_FONTBASE}.G_S_U_B_.ttx \
     ${FEATURE_GSUB_FEA} \
     > table-pwid.tbl 2> table-pwid.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 
 echo "making conversion table (OpenType feature ruby)..."
 ${BINDIR}/make_feature_table \
     table10.tbl ruby ${TTXDIR}/${SRC_FONTBASE}.G_S_U_B_.ttx \
     ${FEATURE_GSUB_FEA} \
     > table-ruby.tbl 2> table-ruby.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 
 echo "making conversion table (OpenType feature vert)..."
 ${BINDIR}/make_feature_table \
     table10.tbl vert ${TTXDIR}/${SRC_FONTBASE}.G_S_U_B_.ttx \
     ${FEATURE_GSUB_FEA} \
     > table-vert.tbl 2> table-vert.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 
 echo "merging convertion tables (OpenType features)..."
 ${BINDIR}/merge_table \
     table-fwid.tbl table10.tbl \
     > table11.tbl 2> table11.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 ${BINDIR}/merge_table \
     table-hwid.tbl table11.tbl \
     > table12.tbl 2> table12.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 ${BINDIR}/merge_table \
     table-pwid.tbl table12.tbl \
     > table13.tbl 2> table13.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 ${BINDIR}/merge_table \
     table-ruby.tbl table13.tbl \
     > table14.tbl 2> table14.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 ${BINDIR}/merge_table \
     table-vert.tbl table14.tbl \
     > table20.tbl 2> table20.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 
 echo "making conversion table (OpenType feature fwid) pass 2..."
 ${BINDIR}/make_feature_table \
     table20.tbl fwid ${TTXDIR}/${SRC_FONTBASE}.G_S_U_B_.ttx \
     ${FEATURE_GSUB_FEA} \
     > table-fwid2.tbl 2> table-fwid2.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 
 echo "making conversion table (OpenType feature hwid) pass 2..."
 ${BINDIR}/make_feature_table \
     table20.tbl hwid ${TTXDIR}/${SRC_FONTBASE}.G_S_U_B_.ttx \
     ${FEATURE_GSUB_FEA} \
     > table-hwid2.tbl 2> table-hwid2.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 
 echo "making conversion table (OpenType feature pwid) pass 2..."
 ${BINDIR}/make_feature_table \
     table20.tbl pwid ${TTXDIR}/${SRC_FONTBASE}.G_S_U_B_.ttx \
     ${FEATURE_GSUB_FEA} \
     > table-pwid2.tbl 2> table-pwid2.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 
 echo "making conversion table (OpenType feature ruby) pass 2..."
 ${BINDIR}/make_feature_table \
     table20.tbl ruby ${TTXDIR}/${SRC_FONTBASE}.G_S_U_B_.ttx \
     ${FEATURE_GSUB_FEA} \
     > table-ruby2.tbl 2> table-ruby2.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 
 echo "making conversion table (OpenType feature vert) pass 2..."
 ${BINDIR}/make_feature_table \
     table20.tbl vert ${TTXDIR}/${SRC_FONTBASE}.G_S_U_B_.ttx \
     ${FEATURE_GSUB_FEA} \
     > table-vert2.tbl 2> table-vert2.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 
 echo "merging convertion tables (OpenType features) pass 2..."
 ${BINDIR}/merge_table \
@@ -205,36 +205,36 @@ ${BINDIR}/merge_table \
 ${BINDIR}/merge_table \
     table-hwid2.tbl table21.tbl \
     > table22.tbl 2> table22.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 ${BINDIR}/merge_table \
     table-pwid2.tbl table22.tbl \
     > table23.tbl 2> table23.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 ${BINDIR}/merge_table \
     table-ruby2.tbl table23.tbl \
     > table24.tbl 2> table24.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 ${BINDIR}/merge_table \
     table-vert2.tbl table24.tbl \
     > table.tbl 2> table.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 
 echo making glyphorder...
 ${BINDIR}/make_glyphorder \
     table.tbl \
     > glyphorder.ttx 2> glyphorder.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 
 echo converting name table...
 ${BINDIR}/conv_name \
     ${SRC_FONTBASE} "${TOOLVER}" ${TTXDIR}/${SRC_FONTBASE}._n_a_m_e.ttx \
     2> name.log | sed -f ${FONT_NAME_SED} > name.ttx \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 echo converting cmap table...
 ${BINDIR}/conv_cmap \
     table.tbl ${TTXDIR}/${SRC_FONTBASE}._c_m_a_p.ttx \
     > cmap.ttx 2> cmap.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 echo converting CFF table...
 ${BINDIR}/conv_CFF \
     ${ROS_R} ${ROS_O} ${ROS_S} \
@@ -246,7 +246,7 @@ if [ -f ${TTXDIR}/${SRC_FONTBASE}.G_D_E_F_.ttx ]; then
     ${BINDIR}/conv_GDEF \
         table.tbl ${TTXDIR}/${SRC_FONTBASE}.G_D_E_F_.ttx \
         > GDEF.ttx 2> GDEF.log \
-       || { echo error; exit 1; }
+        || { echo error; exit 1; }
 else
     echo no GDEF table...
 fi
@@ -254,27 +254,27 @@ echo converting GSUB table...
 ${BINDIR}/conv_GSUB \
     table.tbl ${TTXDIR}/${SRC_FONTBASE}.G_S_U_B_.ttx \
     > GSUB01.ttx 2> GSUB.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 echo converting VORG table...
 ${BINDIR}/conv_VORG \
     table.tbl ${TTXDIR}/${SRC_FONTBASE}.V_O_R_G_.ttx \
     > VORG.ttx 2> VORG.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 echo converting hmtx table...
 ${BINDIR}/conv_mtx \
     table.tbl ${TTXDIR}/${SRC_FONTBASE}._h_m_t_x.ttx \
     > hmtx_conv.ttx 2> hmtx_conv.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 echo fixing widths in hmtx table...
 ${BINDIR}/fix_hmtx \
     ${ROS} table.tbl hmtx_conv.ttx \
     > hmtx_width.ttx 2> hmtx_width.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 echo converting vmtx table...
 ${BINDIR}/conv_mtx \
     table.tbl ${TTXDIR}/${SRC_FONTBASE}._v_m_t_x.ttx \
     > vmtx_conv.ttx 2> vmtx_conv.log \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 
 if [ "${FONT_LANG}" = "JP" ]; then
     echo copying and rotating glyphs in CFF table...
@@ -399,7 +399,7 @@ ln -s ${TTXDIR}/${SRC_FONTBASE}._v_h_e_a.ttx || { echo error; exit 1; }
 echo converting root ttx file...
 sed -f ${BASEDIR}/font_tables.sed ${TTXDIR}/${SRC_FONTBASE}.ttx \
     > output.ttx \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
 
 ttx -b --recalc-timestamp output.ttx \
-   || { echo error; exit 1; }
+    || { echo error; exit 1; }
