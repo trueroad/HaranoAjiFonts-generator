6f920c82c8396b0ed1599c528d95c9cc88fab8b5
Masamichi Hosoda <trueroad@trueroad.jp>
2020-05-23 15:32:33 +0900

    Improve count_conversions.sh

--- count_conversions.sh
+++ count_conversions.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-for x in build/*/table.tbl; do
-    echo "${x} without .notdef"
-    egrep "^[[:digit:]]+[[:space:]][[:digit:]]+" ${x} | wc -l
+BUILDDIR=$1
+
+for x in ${BUILDDIR}/*; do
+    echo "${x}"
+    CONV=$(egrep "^[[:digit:]]+[[:space:]][[:digit:]]+" \
+        ${x}/table.tbl | wc -l)
+    COPY=$(egrep "^aji[[:digit:]]+[[:space:]]aji[[:digit:]]+" \
+        ${x}/copy_and_rotate01.tbl | wc -l)
+    TOTAL=$(expr ${CONV} + ${COPY} + 1)
+    echo "conversion = ${CONV}, copy = ${COPY}, .notdef = 1: total ${TOTAL}"
 done
