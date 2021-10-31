daa9a3e4f595d7c2411f0ef331a18b20cf4a2536
Masamichi Hosoda <trueroad@trueroad.jp>
2021-10-31 15:58:32 +0900

    Use script/show_available_cids.py in count_conversions.sh

--- count_conversions.sh
+++ count_conversions.sh
@@ -2,12 +2,29 @@
 
 BUILDDIR=$1
 
-for x in ${BUILDDIR}/*; do
+if [ ! -d "${BUILDDIR}" ]; then
+    echo "not found"
+    exit
+fi
+
+DIR=$(cd $(dirname $0); pwd)
+SCRIPTDIR=${DIR}/script
+
+for x in ${BUILDDIR}/HaranoAji*; do
     echo "${x}"
-    CONV=$(egrep "^[[:digit:]]+[[:space:]][[:digit:]]+" \
-        ${x}/table.tbl | wc -l)
-    COPY=$(egrep "^aji[[:digit:]]+[[:space:]]aji[[:digit:]]+" \
-        ${x}/copy_and_rotate_do.tbl | wc -l)
-    TOTAL=$(expr ${CONV} + ${COPY} + 1)
-    echo "conversion = ${CONV}, copy = ${COPY}, .notdef = 1: total ${TOTAL}"
+    AVAILABLE_TXT=${x}/available.txt
+    AVAILABLE_LOG=${x}/available.log
+    if [ -e ${AVAILABLE_TXT} ]; then
+        AVAILABLE_TXT=/dev/null
+    fi
+    if [ -e ${AVAILABLE_LOG} ]; then
+        ${SCRIPTDIR}/show_available_cids.py \
+                    ${x}/table.tbl ${x}/copy_and_rotate_do.tbl \
+                    3>&1 1> ${AVAILABLE_TXT} 2>&3
+    else
+        ${SCRIPTDIR}/show_available_cids.py \
+                    ${x}/table.tbl ${x}/copy_and_rotate_do.tbl \
+                    1> ${AVAILABLE_TXT} 2> ${AVAILABLE_LOG}
+        cat ${AVAILABLE_LOG}
+    fi
 done
