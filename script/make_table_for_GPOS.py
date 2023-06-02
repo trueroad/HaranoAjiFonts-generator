af58ff02008755f2bbb2710c52bc50d6c9b6d0ad
Masamichi Hosoda <trueroad@trueroad.jp>
2023-06-02 19:45:41 +0900

    Add docstring to script/make_table_for_GPOS.py

--- script/make_table_for_GPOS.py
+++ script/make_table_for_GPOS.py
@@ -1,39 +1,40 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Harano Aji Fonts generator.
 
-#
-# Harano Aji Fonts generator
-# https://github.com/trueroad/HaranoAjiFonts-generator
-#
-# make_table_for_GPOS.py:
-#   Make conversion table for GPOS.
-#
-# Copyright (C) 2020 Masamichi Hosoda.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-# * Redistributions of source code must retain the above copyright notice,
-#   this list of conditions and the following disclaimer.
-#
-# * Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.
-# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-# SUCH DAMAGE.
-#
+https://github.com/trueroad/HaranoAjiFonts-generator
+
+make_table_for_GPOS.py:
+  Make conversion table for GPOS.
+
+Copyright (C) 2020, 2023 Masamichi Hosoda.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+* Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+"""
 
 import os
 import sys
@@ -44,6 +45,7 @@ import load_table
 
 def load_letterface_table(filename: Union[str, bytes, os.PathLike[Any]]
                           ) -> set[int]:
+    """Load letter_face.tbl."""
     table: set[int] = set()
     f: TextIO
     with open(filename, "r") as f:
@@ -60,6 +62,7 @@ def load_letterface_table(filename: Union[str, bytes, os.PathLike[Any]]
 
 
 def main() -> None:
+    """Do main."""
     if len(sys.argv) != 3:
         print("Usage: make_table_for_GPOS.py (in)table.tbl "
               "(in)leter_face.tbl > (out)table_for_GPOS.tbl")
