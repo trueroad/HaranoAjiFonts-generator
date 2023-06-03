49bd3bde7487385fab48d676e70892621ddc6015
Masamichi Hosoda <trueroad@trueroad.jp>
2023-06-03 20:08:33 +0900

    Refactor script/load_table.py

--- script/load_table.py
+++ script/load_table.py
@@ -41,97 +41,101 @@ import sys
 from typing import Any, TextIO, Union
 
 
-def load_as_dict(filename: Union[str, bytes, os.PathLike[Any]]
-                 ) -> dict[int, int]:
-    """Load table.tbl as dict."""
-    table: dict[int, int] = {}
+#
+# For table.tbl
+#
+
+
+def load_base(filename: Union[str, bytes, os.PathLike[Any]]
+              ) -> tuple[list[tuple[int, int]], int]:
+    """Load table.tbl base."""
+    table: list[tuple[int, int]] = []
+    cid_max: int = -1
     f: TextIO
     with open(filename, 'r') as f:
+        row: int = 0
         line: str
         for line in f:
+            row += 1
             if line.startswith('#'):
                 continue
-            if line.startswith('max'):
-                continue
             items: list[str] = line.split()
-            if len(items) == 2 and \
-               items[0].isdecimal() and items[1].isdecimal():
-                table[int(items[0])] = int(items[1])
-    return table
+            if len(items) == 2 and items[1].isdecimal():
+                cid_out: int = int(items[1])
+                if items[0].isdecimal():
+                    table.append((int(items[0]), cid_out))
+                elif items[0] != 'max':
+                    print('Error: 1st column is not integer (cid_in) or '
+                          "'max'.\n"
+                          f"  row {row}, filename '{str(filename)}',\n"
+                          f"  1st column: '{items[0]}'",
+                          file=sys.stderr)
+                    sys.exit(1)
+                if cid_max < cid_out:
+                    cid_max = cid_out
+            elif len(items) == 1 and items[0].isdecimal():
+                table.append((int(items[0]), -1))
+            else:
+                print(f'Error: Invalid table format.\n'
+                      f"  row {row}, filename '{str(filename)}',\n"
+                      f"  line: '{line}'",
+                      file=sys.stderr)
+                sys.exit(1)
+    return table, cid_max
 
 
 def load_as_list(filename: Union[str, bytes, os.PathLike[Any]]
                  ) -> list[tuple[int, int]]:
-    """Load table.tbl as list."""
+    """Load table.tbl as list without no-conversion AI0 CID."""
     table: list[tuple[int, int]] = []
-    f: TextIO
-    with open(filename, 'r') as f:
-        line: str
-        for line in f:
-            if line.startswith('#'):
-                continue
-            if line.startswith('max'):
-                continue
-            items: list[str] = line.split()
-            if len(items) == 2 and \
-               items[0].isdecimal() and items[1].isdecimal():
-                table.append((int(items[0]), int(items[1])))
+    cid_in: int
+    cid_out: int
+    for cid_in, cid_out in load_base(filename)[0]:
+        if cid_out >= 0:
+            table.append((cid_in, cid_out))
     return table
 
 
 def load_as_list_with_noconv(filename: Union[str, bytes, os.PathLike[Any]]
                              ) -> list[tuple[int, int]]:
     """Load table.tbl as list with no-conversion AI0 CID."""
-    table: list[tuple[int, int]] = []
-    f: TextIO
-    with open(filename, 'r') as f:
-        line: str
-        for line in f:
-            if line.startswith('#'):
-                continue
-            if line.startswith('max'):
-                continue
-            items: list[str] = line.split()
-            if len(items) == 2 and \
-               items[0].isdecimal() and items[1].isdecimal():
-                table.append((int(items[0]), int(items[1])))
-            if len(items) == 1 and items[0].isdecimal():
-                table.append((int(items[0]), -1))
+    return load_base(filename)[0]
+
+
+def load_as_dict(filename: Union[str, bytes, os.PathLike[Any]]
+                 ) -> dict[int, int]:
+    """Load table.tbl as dict."""
+    table: dict[int, int] = {}
+    cid_in: int
+    cid_out: int
+    for cid_in, cid_out in load_base(filename)[0]:
+        if cid_out >= 0:
+            table[cid_in] = cid_out
     return table
 
 
 def load_pre_defined_cid_set(filename: Union[str, bytes, os.PathLike[Any]]
                              ) -> set[int]:
-    """Load table.tbl for pre-defined CID."""
+    """Load table.tbl for pre-defined CID (not including reserved)."""
     s: set[int] = set()
-    table: list[tuple[int, int]] = load_as_list(filename)
-
     cid: int
-    for _, cid in table:
+    for _, cid in load_base(filename)[0]:
         if cid in s:
             print(f'Duplicate: table: {cid}', file=sys.stderr)
-        s.add(cid)
-
+        if cid >= 0:
+            s.add(cid)
     return s
 
 
 def load_pre_defined_cid_max(filename: Union[str, bytes, os.PathLike[Any]]
                              ) -> int:
-    """Load table.tbl for pre-defined CID max."""
-    cid_max: int = -1
-    f: TextIO
-    with open(filename, 'r') as f:
-        line: str
-        for line in f:
-            if line.startswith('#'):
-                continue
-            items: list[str] = line.split()
-            if len(items) == 2 and items[1].isdecimal():
-                cid: int = int(items[1])
-                if cid > cid_max:
-                    cid_max = cid
+    """Load table.tbl for pre-defined CID max (including reserved)."""
+    return load_base(filename)[1]
 
-    return cid_max
+
+#
+# Main
+#
 
 
 def main() -> None:
@@ -140,21 +144,25 @@ def main() -> None:
         print('Usage: load_table.py table.tbl')
         sys.exit(1)
 
-    table_filename: str = sys.argv[1]
-    d: dict[int, int] = load_as_dict(table_filename)
-    lc: list[tuple[int, int]] = load_as_list(table_filename)
-    lnc: list[tuple[int, int]] = load_as_list_with_noconv(table_filename)
-    spd: set[int] = load_pre_defined_cid_set(table_filename)
-
     import pprint
-    print('dict')
-    pprint.pprint(d)
-    print('\nlist')
-    pprint.pprint(lc)
-    print('\nlist with noconv')
-    pprint.pprint(lnc)
-    print('\npre-defined CID set')
-    pprint.pprint(spd)
+
+    table_filename: str = sys.argv[1]
+
+    table_list: list[tuple[int, int]] = load_as_list(table_filename)
+    print('table.tbl list')
+    pprint.pprint(table_list)
+
+    table_dict: dict[int, int] = load_as_dict(table_filename)
+    print('\ntable.tbl dict')
+    pprint.pprint(table_dict)
+
+    table_set: set[int] = load_pre_defined_cid_set(table_filename)
+    print('\ntable.tbl pre-defined CID set (not including reserved)')
+    pprint.pprint(table_set)
+
+    cid_max: int = load_pre_defined_cid_max(table_filename)
+    print('\ntable.tbl pre-defined CID max (including reserved)')
+    print(cid_max)
 
 
 if __name__ == '__main__':
