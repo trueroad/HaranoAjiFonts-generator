98528dfef29ade2f8b4e76814a4517bff861665f
Masamichi Hosoda <trueroad@trueroad.jp>
2023-06-04 17:17:56 +0900

    Add docstring to script/set_mtx_width_height.py

--- script/set_mtx_width_height.py
+++ script/set_mtx_width_height.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python3
-#
-# Harano Aji Fonts generator
-# https://github.com/trueroad/HaranoAjiFonts-generator
-#
-# set_mtx_width_height.py:
-#   Set hmtx width or vmtx height for pwid or pwidvert glyphs.
-#
-# Copyright (C) 2020, 2023 Masamichi Hosoda.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-# * Redistributions of source code must retain the above copyright notice,
-#   this list of conditions and the following disclaimer.
-#
-# * Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.
-# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-# SUCH DAMAGE.
-#
+# -*- coding: utf-8 -*-
+"""
+Harano Aji Fonts generator.
+
+https://github.com/trueroad/HaranoAjiFonts-generator
+
+set_mtx_width_height.py:
+  Set hmtx width or vmtx height for pwid or pwidvert glyphs.
+
+Copyright (C) 2020, 2023 Masamichi Hosoda.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+* Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+"""
 
 import sys
 from typing import Optional, TextIO
@@ -40,6 +42,7 @@ import xml.etree.ElementTree as ET
 
 
 def load_table(file: str) -> dict[str, int]:
+    """Load {width|height}.tbl."""
     table: dict[str, int] = {}
     f: TextIO
     with open(file, "r") as f:
@@ -58,6 +61,7 @@ def load_table(file: str) -> dict[str, int]:
 
 
 def main() -> None:
+    """Do main."""
     if len(sys.argv) <= 3:
         print("Usage: set_mtx_width_height.py {width|height}.tbl "
               "INPUT.{h|v}mtx.ttx OUTPUT.{h|v}mtx.ttx")
