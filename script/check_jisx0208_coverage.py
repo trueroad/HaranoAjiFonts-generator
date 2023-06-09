17289cfb44fdb291c5e32715d5dbae1dd0c67dc5
Masamichi Hosoda <trueroad@trueroad.jp>
2023-06-09 22:36:29 +0900

    Add docstring to script/check_jisx0208_coverage.py

--- script/check_jisx0208_coverage.py
+++ script/check_jisx0208_coverage.py
@@ -1,39 +1,40 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Harano Aji Fonts generator.
 
-#
-# Harano Aji Fonts generator
-# https://github.com/trueroad/HaranoAjiFonts-generator
-#
-# check_jisx0208_coverage:
-#   Check JISX0208-SourceHan-Mapping.txt coverage.
-#
-# Copyright (C) 2021 Masamichi Hosoda.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-# * Redistributions of source code must retain the above copyright notice,
-#   this list of conditions and the following disclaimer.
-#
-# * Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.
-# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-# SUCH DAMAGE.
-#
+https://github.com/trueroad/HaranoAjiFonts-generator
+
+check_jisx0208_coverage:
+  Check JISX0208-SourceHan-Mapping.txt coverage.
+
+Copyright (C) 2021, 2023 Masamichi Hosoda.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+* Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+"""
 
 from typing import List
 import sys
@@ -45,6 +46,7 @@ import load_table
 # Adobe-Japan1-6 vs Source Han
 # https://ccjktype.fonts.adobe.com/2019/03/aj16-vs-source-han.html
 def load_jisx0208(file: str) -> List[int]:
+    """Load JISX0208-SourceHan-Mapping.txt."""
     retval: List[int] = []
     with open(file, "r") as f:
         for line in f:
@@ -65,6 +67,7 @@ def load_jisx0208(file: str) -> List[int]:
 
 
 def main() -> None:
+    """Do main."""
     if len(sys.argv) != 3:
         print('Usage: check_jisx0208_coverage.py '
               'JISX0208-SourceHan-Mapping.txt '
